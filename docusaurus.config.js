effccb017447f56b84576540060793897795b2c6
rugamoto <apetoshi.rugamoto@gmail.com>
2022-06-15 00:01:54 +0100

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "Socket V3 SDK",
   tagline: "Dinosaurs are cool",
   url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  baseUrl: "/socket-v2-docs/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "rugamoto", // Usually your GitHub org/user name.
-  projectName: "v2-sdk-docs", // Usually your repo name.
+  projectName: "socket-v2-docs", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
